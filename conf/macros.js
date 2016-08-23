4c4beaa9d5b1a4201888c8f6b158156f896fc79a
Sean K. Friese <skfriese@gmail.com>
2016-08-23 13:07:41 -0400

    reformats macros

--- conf/macros.js
+++ conf/macros.js
@@ -4,43 +4,14 @@ var LMSDiagMacros = [
   { 
     label: "Gets and sets some values, sets SCO to completed",
     steps: [
-      {
-        type: "get",
-        key: "cmi.core.lesson_status"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "incomplete"
-      },
-      {
-        type: "get",
-        key: "cmi.core.lesson_location"
-      },
-      {
-        type: "get",
-        key: "cmi.suspend_data"
-      },
-      {
-        type: "set",
-        key: "cmi.suspend_data",
-        val: "test123"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_location",
-        val: "page_af87f1iu2g4189724byq8we7sd897f9s"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "completed"
-      },
-      {
-        type: "set",
-        key: "cmi.core.session_time",
-        val: function(){ return diag.getElapsedTime(); }
-      }
+      { type: "get", key: "cmi.core.lesson_status"}, 
+      { type: "set", key: "cmi.core.lesson_status", val: "incomplete"}, 
+      { type: "get", key: "cmi.core.lesson_location"}, 
+      { type: "get", key: "cmi.suspend_data"}, 
+      { type: "set", key: "cmi.suspend_data", val: "test123"}, 
+      { type: "set", key: "cmi.core.lesson_location", val: "page_af87f1iu2g4189724byq8we7sd897f9s"}, 
+      { type: "set", key: "cmi.core.lesson_status", val: "completed"}, 
+      { type: "set", key: "cmi.core.session_time", val: function(){ return diag.getElapsedTime(); } }
     ]
   },
 
@@ -48,58 +19,17 @@ var LMSDiagMacros = [
   {
     label: "Gets and sets some values, passes SCO with score",
     steps: [
-      {
-        type: "get",
-        key: "cmi.core.lesson_status"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "incomplete"
-      },
-      {
-        type: "get",
-        key: "cmi.suspend_data"
-      },
-      {
-        type: "set",
-        key: "cmi.suspend_data",
-        val: "test789"
-      },
-      {
-        type: "get",
-        key: "cmi.core.lesson_location"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_location",
-        val: "page_4279814g2ui1f78fas9f798ds7ew8qyb"
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.min",
-        val: 0
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.max",
-        val: 100
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.raw",
-        val: 85
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "passed"
-      },
-      {
-        type: "set",
-        key: "cmi.core.session_time",
-        val: function(){ return diag.getElapsedTime(); }
-      }
+      { type: "get", key: "cmi.core.lesson_status"},
+      { type: "set", key: "cmi.core.lesson_status", val: "incomplete"}, 
+      { type: "get", key: "cmi.suspend_data"}, 
+      { type: "set", key: "cmi.suspend_data", val: "test789"}, 
+      { type: "get", key: "cmi.core.lesson_location"}, 
+      { type: "set", key: "cmi.core.lesson_location", val: "page_4279814g2ui1f78fas9f798ds7ew8qyb"}, 
+      { type: "set", key: "cmi.core.score.min", val: 0 }, 
+      { type: "set", key: "cmi.core.score.max", val: 100 }, 
+      { type: "set", key: "cmi.core.score.raw", val: 85 }, 
+      { type: "set", key: "cmi.core.lesson_status", val: "passed"}, 
+      { type: "set", key: "cmi.core.session_time", val: function(){ return diag.getElapsedTime(); } }
     ]
   },
 
@@ -107,107 +37,34 @@ var LMSDiagMacros = [
   {
     label: "Gets and sets some values, fails SCO with score",
     steps: [
-      {
-        type: "get",
-        key: "cmi.core.lesson_status"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "incomplete"
-      },
-      {
-        type: "get",
-        key: "cmi.suspend_data"
-      },
-      {
-        type: "set",
-        key: "cmi.suspend_data",
-        val: "test789"
-      },
-      {
-        type: "get",
-        key: "cmi.core.lesson_location"
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.min",
-        val: 0
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.max",
-        val: 100
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.raw",
-        val: 45
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "failed"
-      },
-      {
-        type: "set",
-        key: "cmi.core.session_time",
-        val: function(){ return diag.getElapsedTime(); }
-      }
+      { type: "get", key: "cmi.core.lesson_status"}, 
+      { type: "set", key: "cmi.core.lesson_status", val: "incomplete"}, 
+      { type: "get", key: "cmi.suspend_data"}, 
+      { type: "set", key: "cmi.suspend_data", val: "test789"}, 
+      { type: "get", key: "cmi.core.lesson_location"}, 
+      { type: "set", key: "cmi.core.score.min", val: 0 }, 
+      { type: "set", key: "cmi.core.score.max", val: 100 }, 
+      { type: "set", key: "cmi.core.score.raw", val: 45 }, 
+      { type: "set", key: "cmi.core.lesson_status", val: "failed"}, 
+      { type: "set", key: "cmi.core.session_time", val: function(){ return diag.getElapsedTime(); } }
     ]
   },
 
   // 3
   {
-    label: "Sets interaction data, passes SCO with score",
+    label: "Sets interaction and objectives data, passes SCO with score",
     steps: [
-      {
-        type: "get",
-        key: "cmi.core.lesson_status"
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "incomplete"
-      },
-      {
-        type: "get",
-        key: "cmi.suspend_data"
-      },
-      {
-        type: "set",
-        key: "cmi.suspend_data",
-        val: "test789"
-      },
-      {
-        type: "get",
-        key: "cmi.core.lesson_location"
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.min",
-        val: 0
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.max",
-        val: 100
-      },
-      {
-        type: "set",
-        key: "cmi.core.score.raw",
-        val: 45
-      },
-      {
-        type: "set",
-        key: "cmi.core.lesson_status",
-        val: "failed"
-      },
-      {
-        type: "set",
-        key: "cmi.core.session_time",
-        val: function(){ return diag.getElapsedTime(); }
-      }
+      { type: "get", key: "cmi.core.lesson_status"}, 
+      { type: "set", key: "cmi.core.lesson_status", val: "incomplete"}, 
+      { type: "get", key: "cmi.suspend_data"}, 
+      { type: "set", key: "cmi.suspend_data", val: "test789"}, 
+      { type: "get", key: "cmi.core.lesson_location"}, 
+      { type: "set", key: "cmi.core.score.min", val: 0 }, 
+      { type: "set", key: "cmi.core.score.max", val: 100 }, 
+      { type: "set", key: "cmi.core.score.raw", val: 45 }, 
+      { type: "set", key: "cmi.core.lesson_status", val: "failed"}, 
+      { type: "set", key: "cmi.core.session_time", val: function(){ return diag.getElapsedTime(); } }
     ]
   }
+  
 ];
